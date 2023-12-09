a6b29bbfbfcd3fd9b32647a5459ba681d5b9a844
vivostar <lishuaipeng651@gmail.com>
2023-12-09 22:59:44 +0800

    modify to work well here

--- jupyter/run.sh
+++ jupyter/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "Starting jupyter notebook..."
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' --notebook-dir=/notebook
+# CMD在docker中必须前台一直启动才代表其启动
+# while true
+# do
+#   sleep 3
+#   echo "go"
+#   # loop infinitely
+# done
+jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token='' --notebook-dir=/home/jupyter/notebook
