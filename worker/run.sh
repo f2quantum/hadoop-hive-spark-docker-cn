b8d4fb6ae918bf0e2f138abb4d50d99c9fe9d073
vivostar <lishuaipeng651@gmail.com>
2023-12-10 22:54:33 +0800

    improve

--- worker/run.sh
+++ worker/run.sh
@@ -8,3 +8,9 @@ yarn --daemon start nodemanager
 
 echo "Starting Spark slave node..."
 spark-class org.apache.spark.deploy.worker.Worker "spark://master:7077"
+# 使用yarn模式不启动spark worker
+# while true
+# do
+#   sleep 10
+#   # loop infinitely
+# done
